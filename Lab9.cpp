ce58bc158e3270c9397c487fce80143cd7ff7fd9
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2023-01-02 18:55:25 -0500

    Create Lab9.cpp

---
+++ Lab9.cpp
@@ -0,0 +1,14 @@
+// Write a program that prompts a user to enter a positive integer 
+// to begin the algorithm for the Collatz conjecture. The program 
+// will use a while loop to print each term (hailstone) and make 
+// repeated decisions to determine which transformation to apply 
+// to each value of the sequence according to the conjecture algorithm 
+// until it reaches 1.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
