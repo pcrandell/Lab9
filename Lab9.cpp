e48b376df01fcb8040e2c0958b5854a9ab669744
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2023-10-02 19:04:31 -0400

    Update Lab9.cpp

--- Lab9.cpp
+++ Lab9.cpp
@@ -1,14 +1,13 @@
-// Write a program that prompts a user to enter a positive integer 
-// to begin the algorithm for the Collatz conjecture. The program 
-// will use a while loop to print each term (hailstone) and make 
-// repeated decisions to determine which transformation to apply 
-// to each value of the sequence according to the conjecture algorithm 
-// until it reaches 1.
+// The Fibonacci sequence is a series of numbers in which each number is the sum 
+// of the two that precede it. Starting at 0 and 1, the sequence looks like this: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, and so on forever. 
+// The Fibonacci sequence can be described using a mathematical equation: XN+2 = XN+1 + XN
+
+// Write a program that will prompt the user for an integer that will define how many values in the Fibonacci sequence to print to the screen.
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    
+
     return 0;
 }
